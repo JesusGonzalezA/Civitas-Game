8002122de3fb6ee2cd9e590793a06b8ec1deff76
Jesús González Álvarez <jesusgranada99@gmail.com>
2019-11-13 16:58:47 +0100

    Tablero completo

--- src/civitas/CivitasJuego.java
+++ src/civitas/CivitasJuego.java
@@ -117,7 +117,7 @@ public class CivitasJuego {
         indiceJugadorActual = Dado.getInstance().quienEmpieza(nombres.size());
         
         //Creo mazo e inicializo tablero(también lo crea) y mazo
-        mazo = new MazoSorpresas();
+        mazo = new MazoSorpresas(true);
         inicializaTablero(mazo);
         inicializaMazoSorpresas(tablero);
     }
@@ -219,13 +219,13 @@ public class CivitasJuego {
         
         
 ////        //Añado casillas
-//        tablero.añadeCasilla(new Casilla(mazo,"Caja de Comunidad"));
-//        tablero.añadeCasilla(new Casilla("Descanso"));
-//        tablero.añadeCasilla(new Casilla(new TituloPropiedad("Murcia",
-//                                        25.0f,50.0f,1000.0f,2000.0f,500.0f)));
-        tablero.añadeCasilla(new Casilla("Impuesto por ser tan guapo", 8500.0f));
-//        tablero.añadeCasilla(new Casilla(new TituloPropiedad("Paseo del Prado",
-//                                        50.0f,100.0f,2000.0f,4000.0f,1000.0f)));
+        tablero.añadeCasilla(new Casilla(mazo,"Caja de Comunidad"));
+        tablero.añadeCasilla(new Casilla("Descanso"));
+        tablero.añadeCasilla(new Casilla(new TituloPropiedad("Murcia",
+                                        25.0f,50.0f,1000.0f,2000.0f,500.0f)));
+        tablero.añadeCasilla(new Casilla("Impuesto por ser tan guapo", 1500.0f));
+        tablero.añadeCasilla(new Casilla(new TituloPropiedad("Paseo del Prado",
+                                        50.0f,100.0f,2000.0f,4000.0f,1000.0f)));
         tablero.añadeJuez();
     }
     
